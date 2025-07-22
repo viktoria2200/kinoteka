641d0a436b89ed245ae3b7c31c0b36773a06051b
viktoria2200 <volkova31122@yandex.ru>
2025-07-22 23:11:32 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,33 @@
-const CACHE_NAME = 'kinoteka-cache-v1';
+const CACHE_NAME = 'kinoteka-cache-v4';
 const urlsToCache = [
   '/kinoteka/',
   '/kinoteka/index.html',
   '/kinoteka/film.html',
   '/kinoteka/serial.html',
   '/kinoteka/stil.css',
+  '/kinoteka/manifest.json',
   '/kinoteka/icons/icon-192x192.png',
-  '/kinoteka/icons/icon-512x512.png'
+  '/kinoteka/icons/icon-512x512.png',
+  'https://aif-s3.aif.ru/images/025/364/a9a0f1974d40b12e43789b6327ba148d.jpg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
   );
 });
 
