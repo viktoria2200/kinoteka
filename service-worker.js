c08527d175ea999b5f8f6f7ae0d2deea440b4f75
viktoria2200 <volkova31122@yandex.ru>
2025-07-22 23:31:12 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,44 @@
-const CACHE_NAME = 'kinoteka-cache-v6'; // Новая версия!
+const CACHE_NAME = 'kinoteka-cache-v8';
 const urlsToCache = [
-  './',
-  './index.html',
-  './film.html',
-  './serial.html',
-  './stil.css',
-  './manifest.json',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+    './',
+    './index.html',
+    './film.html',
+    './serial.html',
+    './stil.css',
+    './manifest.json',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
+    'https://aif-s3.aif.ru/images/025/364/a9a0f1974d40b12e43789b6327ba148d.jpg'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Кеширую ресурсы');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(name => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(name => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
-  self.clients.claim(); 
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-self.addEventListener('install', event => {
-  console.log('Installing SW...');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Caching files');
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => console.log('All files cached'))
-      .catch(err => console.error('Caching failed:', err))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
 });
